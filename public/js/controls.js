1931a0c7319085cf9970b6458cb152ba2523efa9
TKatra <tobiaskatra@hotmail.com>
2016-10-08 01:51:51 +0200

    Removed comments

--- public/js/controls.js
+++ public/js/controls.js
@@ -5,26 +5,21 @@ function initializeControls() {
   left.press = function() {
     //goes right, turns to up
     if (playerSnake.vx > 0) {
-      console.log("goes right, turns to up");
-
       playerSnake.vx = 0;
       playerSnake.vy = -playerSpeed;
     }
     //goes up, turns to left
     else if(playerSnake.vy < 0) {
-      console.log("goes up, turns to left");
       playerSnake.vx = -playerSpeed;
       playerSnake.vy = 0;
     }
     //goes left, turns to down
     else if(playerSnake.vx < 0) {
-      console.log("goes left, turns to down");
       playerSnake.vx = 0;
       playerSnake.vy = playerSpeed;
     }
     //goes down, turns to right
     else if(playerSnake.vy > 0) {
-      console.log("goes down, turns to right");
       playerSnake.vx = playerSpeed;
       playerSnake.vy = 0;
     }
@@ -33,25 +28,21 @@ function initializeControls() {
   right.press = function() {
     //goes right, turns to down
     if (playerSnake.vx > 0) {
-      console.log("goes right, turns to down");
       playerSnake.vx = 0;
       playerSnake.vy = playerSpeed;
     }
     //goes down, turns to left
     else if(playerSnake.vy > 0) {
-      console.log("goes down, turns to left");
       playerSnake.vx = -playerSpeed;
       playerSnake.vy = 0;
     }
     //goes left, turns to up
     else if(playerSnake.vx < 0) {
-      console.log("goes left, turns to up");
       playerSnake.vx = 0;
       playerSnake.vy = -playerSpeed;
     }
     //goes up, turns to right
     else if(playerSnake.vy < 0) {
-      console.log("goes up, turns to right");
       playerSnake.vx = playerSpeed;
       playerSnake.vy = 0;
     }
