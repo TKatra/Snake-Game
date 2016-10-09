dd16167bf29672b0dcf51c6522195bdb37d515bf
TKatra <tobiaskatra@hotmail.com>
2016-10-09 17:43:02 +0200

    Fixed a bug with the random number generator

--- public/js/miscFunctions.js
+++ public/js/miscFunctions.js
@@ -3,6 +3,10 @@ function resizeRenderer() {
   createBorders();
 }
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function loadProgressHandler(loader, resource) {
   console.log("Loading: " + resource.url);
   console.log("Progress: " + loader.progress + "%");
