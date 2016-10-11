3f194ff8e1fc22a3ef28b4b0c6aabc759fcaaf8c
TKatra <tobiaskatra@hotmail.com>
2016-10-11 11:40:22 +0200

    Changed the color of the food

--- public/js/food.js
+++ public/js/food.js
@@ -1,7 +1,7 @@
 function createFood() {
   food = new Graphics();
-  food.beginFill(0x66CCFF, 0.5);
-  food.lineStyle(2, 0xFF3300, 1);
+  food.beginFill(0xf4eb00);
+  food.lineStyle(2, 0x8e8157);
   food.drawCircle(0, 0, 5);
   food.x = getRandomInt(foodMargin, maxFoodX);
   food.y = getRandomInt(foodMargin, maxFoodY);
