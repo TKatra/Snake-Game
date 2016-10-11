8d9c2cbf4460126a5080c24df250c30e849457ef
TKatra <tobiaskatra@hotmail.com>
2016-10-11 13:26:48 +0200

    Improved the comments in the global file

--- public/js/global.js
+++ public/js/global.js
@@ -7,34 +7,42 @@ var resources = PIXI.loader.resources;
 var TextureCache = PIXI.utils.TextureCache;
 var Texture = PIXI.Texture;
 var Sprite = PIXI.Sprite;
-var spriteSheet;
-var state;
-var currentState;
 
 //Foundation
 var gameArea;
 var renderer;
+var state;
+var currentState;
+
+//Containers
 var stage = new Container();
 var stageBackground = new Container();
 var startScreen = new Container();
-var startImage;
 var gameOverScreen = new Container();
+
+//Graphics
+var spriteSheet;
+var startImage;
 var gameOverImage;
 
-//Gameplay
+//Player Snake
 var playerSnake;
 var playerSpeed = 5;
 var playerTail = [];
 var playerMaxLength = 5;
 var tailFrameTick = 0;
 
+//Food
 var food;
 var foodMargin = 25;
 var maxFoodX;
 var maxFoodY;
+
+//Points
 var highScore = 1500;
 var points;
 
+//Borders
 var topBorder;
 var leftBorder;
 var rightBorder;
